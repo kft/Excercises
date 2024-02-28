1cd23e28ae83bcb4b2daceb5f87393d4d4055664
Kristian F. Thomsen <kft@energinet.dk>
2024-02-28 19:05:13 +0100

    Empty

--- Sorting/SortingTests.cs
+++ Sorting/SortingTests.cs
@@ -5,5 +5,11 @@ public class SortingTests
     [Fact]
     public void Can_sort_an_array()
     {
+        Assert.Equal(Array.Empty<int>(), Sort(Array.Empty<int>()));
+    }
+
+    private IEnumerable<int> Sort(int[] listToSort)
+    {
+        return new List<int>();
     }
 }
