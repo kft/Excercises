329dbcdb62afb073c467f7497ab9e4db60323501
Kristian F. Thomsen <kft@energinet.dk>
2024-02-28 19:23:21 +0100

    Three numbers

--- Sorting/SortingTests.cs
+++ Sorting/SortingTests.cs
@@ -10,18 +10,23 @@ public class SortingTests
         Assert.Equal(Array.Empty<int>(), Sort(Array.Empty<int>()));
         Assert.Equal(new[] {1}, Sort([1]));
         Assert.Equal(new[] {1, 2}, Sort([2, 1]));
+        Assert.Equal(new[] {1, 2, 3}, Sort([2, 1, 3]));
     }
 
     private IEnumerable<int> Sort(int[] listToSort)
     {
         if (listToSort.Length > 1)
         {
-            if (listToSort[0] > listToSort[1])
+            for (var firstIndex = 0; firstIndex < listToSort.Length - 1; firstIndex++)
             {
-                var firstIndex = listToSort[0];
-                var secondIndex = listToSort[1];
-                listToSort[0] = secondIndex;
-                listToSort[1] = firstIndex;
+                var secondIndex = firstIndex + 1;
+                if (listToSort[firstIndex] > listToSort[secondIndex])
+                {
+                    var firstValue = listToSort[firstIndex];
+                    var secondValue = listToSort[secondIndex];
+                    listToSort[firstIndex] = secondValue;
+                    listToSort[secondIndex] = firstValue;
+                }
             }
         }
         return listToSort;
