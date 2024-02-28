a8f33fe0ab4573c419829e51d4075e7fd3421378
Kristian F. Thomsen <kft@energinet.dk>
2024-02-28 19:43:18 +0100

    Refactor parameter

--- Sorting/SortingTests.cs
+++ Sorting/SortingTests.cs
@@ -6,16 +6,16 @@ public class SortingTests
     public void Can_sort_an_array()
     {
         Assert.Equal(Array.Empty<int>(), Sort(Array.Empty<int>()));
-        Assert.Equal([1], Sort([1]));
-        Assert.Equal([1, 2], Sort([2, 1]));
-        Assert.Equal([1, 2, 3], Sort([2, 1, 3]));
-        Assert.Equal([1, 2, 3], Sort([3, 2, 1]));
-        Assert.Equal([1, 2, 3, 4, 5], Sort([1, 3, 2, 5, 4]));
-        Assert.Equal([3, 5, 5, 6, 7, 10, 34], Sort([6, 3, 5, 5, 7, 10, 34]));
-        Assert.Equal([3, 5, 5, 7, 10, 34, 34], Sort([34, 3, 5, 5, 7, 10, 34]));
+        Assert.Equal([1], Sort(1));
+        Assert.Equal([1, 2], Sort(2, 1));
+        Assert.Equal([1, 2, 3], Sort(2, 1, 3));
+        Assert.Equal([1, 2, 3], Sort(3, 2, 1));
+        Assert.Equal([1, 2, 3, 4, 5], Sort(1, 3, 2, 5, 4));
+        Assert.Equal([3, 5, 5, 6, 7, 10, 34], Sort(6, 3, 5, 5, 7, 10, 34));
+        Assert.Equal([3, 5, 5, 7, 10, 34, 34], Sort(34, 3, 5, 5, 7, 10, 34));
     }
 
-    private IEnumerable<int> Sort(int[] listToSort)
+    private IEnumerable<int> Sort(params int[] listToSort)
     {
         if (listToSort.Length <= 1) return listToSort;
         for (var limit = listToSort.Length - 1; limit > 0; limit--)
