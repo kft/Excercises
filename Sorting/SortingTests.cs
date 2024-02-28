ffbfc1f995cc1e5e953dcf21956f397b1a1cda7d
Kristian F. Thomsen <kft@energinet.dk>
2024-02-28 19:11:16 +0100

    Two numbers

--- Sorting/SortingTests.cs
+++ Sorting/SortingTests.cs
@@ -9,10 +9,21 @@ public class SortingTests
     {
         Assert.Equal(Array.Empty<int>(), Sort(Array.Empty<int>()));
         Assert.Equal(new[] {1}, Sort([1]));
+        Assert.Equal(new[] {1, 2}, Sort([2, 1]));
     }
 
     private IEnumerable<int> Sort(int[] listToSort)
     {
+        if (listToSort.Length > 1)
+        {
+            if (listToSort[0] > listToSort[1])
+            {
+                var firstIndex = listToSort[0];
+                var secondIndex = listToSort[1];
+                listToSort[0] = secondIndex;
+                listToSort[1] = firstIndex;
+            }
+        }
         return listToSort;
     }
 }
